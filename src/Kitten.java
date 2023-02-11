81d854ea7ade512cead93f7eef28d76c522c5f4d
Your Name <you@example.com>
2023-02-11 19:53:33 +0500

    Новое что-то с котятами.

---
+++ src/Kitten.java
@@ -0,0 +1,5 @@
+public interface Kitten
+{
+    void sound();
+    void comeToMe();
+}
---
+++ src/MaineCoon.java
@@ -0,0 +1,23 @@
+/**
+ * Кошка
+ *
+ * @author Violetta
+ */
+public class MaineCoon implements Kitten
+{
+    int weight = 0;
+    int lengthOfTale = 0;
+
+    @Override
+    public void sound()
+    {
+
+    }
+
+    @Override
+    public void comeToMe()
+    {
+
+    }
+
+}
